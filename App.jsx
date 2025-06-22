627372f6d2ffe7e7a177d569aa6d81d310f3e8f0
remler1000 <eremler1000@gmail.com>
2025-06-22 19:07:09 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '40px', fontFamily: 'sans-serif' }}>
+      <h1>🌍 Geo Quiz App</h1>
+      <p>האפליקציה נטענה בהצלחה!</p>
+    </div>
+  );
+}
